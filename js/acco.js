1ea54dda0e1395015704c9f9eb6543b1bfeed3a3
Georgios Tsopozidis <gtsopozidis@gmail.com>
2020-08-25 18:46:08 +0300

    Accordeo vertical fixed

--- js/acco.js
+++ js/acco.js
@@ -6,19 +6,19 @@ const openItem = item => {
 
   container.addClass("active");
   contentBlock.height(reqHeight);
-}
+};
 
 const closeEveryItem = container => {
-  const items = container.find('.team__content');
+  const items = container.find(".team__content");
   const itemContainer = container.find(".team__item");
 
   itemContainer.removeClass("active");
   items.height(0);
-}
+};
 
-$('.team__title').click((e) => {
+$(".team__title").click((e) => {
   const $this = $(e.currentTarget);
-  const container = $this.closest('.team');
+  const container = $this.closest(".team");
   const elemContainer = $this.closest(".team__item");
 
   if (elemContainer.hasClass("active")) {
